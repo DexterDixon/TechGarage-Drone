ff55d675bf74787773a7714162fa30bbab016808
Dexter Dixon <42393991+DexterDixon@users.noreply.github.com>
2019-09-07 15:58:14 -0400

    Added examples

---
+++ Comand_List.py
@@ -0,0 +1,27 @@
+"""
+Distance can be 1-500 in centimeters
+
+drone.move_forward(distance)
+drone.move_backward(distance)
+drone.move_right(distance)
+drone.move_left(distance)
+drone.move_up(distance)
+drone.move_down(distance)
+
+Degrees can be 1-360 clock or counter clockwise
+
+drone.rotate_cw(degrees)
+drone.rotate_ccw(degrees)
+
+Speed can be 1-100 centimeters/second
+
+drone.set_speed(speed)
+
+Get current speed in KPH or MPH: drone.get_speed()
+
+Get percent battery life remaining: drone.get_battery()
+
+Get elapsed flight time in seconds: drone.get_flight_time()
+"""
+
+
---
+++ Emergency_Land.py
@@ -0,0 +1,10 @@
+import tello
+import time
+drone = tello.Tello('192.168.10.2', 8888, imperial=False)
+
+
+drone.land()
+
+
+
+
